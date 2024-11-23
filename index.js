4f9d0bc5389cc765ca9846a0ffd5528d2edb4cae
abu bakar <abubakar850772@gmail.com>
2024-11-23 10:54:15 +0600

    add endpoint to delete product

--- index.js
+++ index.js
@@ -229,6 +229,15 @@ async function run() {
 			res.send({ result, update });
 		});
 
+		// add endpoint to delete product
+
+		app.delete("/product/:id", verifyToken, async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await productCollection.deleteOne(query);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		// await client.db("admin").command({ ping: 1 });
 		// console.log(
